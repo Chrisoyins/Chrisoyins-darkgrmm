d849f305b387d749056a710797a0b36116f6779d
Chrisoyins <110850404+Chrisoyins@users.noreply.github.com>
2026-08-11 01:32:30 +0100

    Add DemoBadge component for demo/sandbox mode

---
+++ src/src/src/src/components/DemoBadge.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function DemoBadge() {
+  return (
+    <div className="fixed top-4 right-4 z-50">
+      <div className="px-3 py-1 bg-yellow-400 text-black rounded shadow">DEMO / SANDBOX MODE</div>
+    </div>
+  );
+}
